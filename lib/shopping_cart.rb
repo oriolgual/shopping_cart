f320ec674ff6c9961c8896c0b49e704aa34206d2
Oriol Gual <oriol.gual@gmail.com>
2014-11-03 11:25:36 +0100

    Blank slate

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -9,21 +9,8 @@ module ShoppingCart
     end
 
     def total
-      items_total - loyalty_discount
     end
 
-    private
-    def loyalty_discount
-      return 0 unless @extra_loyal_customer
-
-      items_total * 0.1
-    end
-
-    def items_total
-      @items_total ||= @items.inject(0) do |total, item|
-        total += (item.price - item.discount(@shopping_day))
-      end
-    end
   end
 
   class Item
@@ -35,13 +22,6 @@ module ShoppingCart
     end
 
     def discount(shopping_day)
-      if [6,0].include?(shopping_day.wday) && @name == 'Banana'
-        return @price * 0.1
-      elsif @name == 'Vacuum cleaner' && @price > 100
-        return @price * 0.05
-      end
-
-      0
     end
   end
 end

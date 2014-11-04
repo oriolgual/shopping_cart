529de6dd902324623b813c7972878eaa45291c97
Oriol Gual <oriol.gual@gmail.com>
2014-11-04 15:10:23 +0100

    USe proper product classes

--- spec/lib/shopping_cart/cart_spec.rb
+++ spec/lib/shopping_cart/cart_spec.rb
@@ -4,10 +4,10 @@ module ShoppingCart
   describe Cart do
     let(:products) do
       [
-        Product.new('Banana', 10),
+        Fruit.new('Banana', 10),
         Product.new('Orange juice', 10),
         Product.new('Rice', 1),
-        Product.new('Vacuum cleaner', 150),
+        Houseware.new('Vacuum cleaner', 150),
         Product.new('Anchovies', 2),
       ]
     end
@@ -26,7 +26,7 @@ module ShoppingCart
 
     describe 'total' do
       it 'sums the prices of the products in the cart' do
-        expect(cart.total).to eq(173)
+        expect(cart.total).to eq(165.50)
       end
 
       context "when it is a extra loyal customer" do
@@ -37,17 +37,17 @@ module ShoppingCart
         context "when it has bought more than 5 products" do
           let(:products) do
             [
-              Product.new('Banana', 10),
+              Fruit.new('Banana', 10),
               Product.new('Orange juice', 10),
               Product.new('Rice', 1),
               Product.new('Rice', 1),
-              Product.new('Vacuum cleaner', 150),
+              Houseware.new('Vacuum cleaner', 150),
               Product.new('Anchovies', 2),
             ]
           end
 
           it 'has an extra 10% discount' do
-            expect(cart.total).to eq(156.6)
+            expect(cart.total).to eq(149.85)
           end
         end
       end

07080f28a19e63e47c54047764d2f739fd303d07
Oriol Gual <oriol.gual@gmail.com>
2014-11-04 15:19:30 +0100

    Refactor

--- lib/shopping_cart/cart.rb
+++ lib/shopping_cart/cart.rb
@@ -15,17 +15,21 @@ module ShoppingCart
     end
 
     def total
-      @products.inject(0) do |total, product|
-        total += product.discounted_price(@shopping_day)
-      end * (1 - loyalty_discount)
+      total_amount - loyalty_discount
     end
 
     private
 
+    def total_amount
+      @total_amount ||= @products.inject(0) do |total, product|
+        total += product.discounted_price(@shopping_day)
+      end
+    end
+
     def loyalty_discount
       return 0 if !@customer.loyal? || count < 5
 
-      0.1
+      total_amount * 0.1
     end
   end
 end
